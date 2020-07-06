bd7e2bd9b64bf7ab8c45acf21dfa20b00b9e468c
Gianmarco Liaci <liacigianmarco@gmail.com>
2020-07-06 20:59:02 +0200

    index and initial course

---
+++ src/app/core/models/index.ts
@@ -0,0 +1,9 @@
+
+export * from './assignment.model';
+export * from './course.model';
+export * from './modelVm.model';
+export * from './solution.model';
+export * from './student.model';
+export * from './teacher.model';
+export * from './team.model';
+export * from './vm.model';
---
+++ src/app/core/services/index.ts
@@ -0,0 +1,9 @@
+
+export * from './assignment.service';
+export * from './course.service';
+export * from './model-vm.service';
+export * from './solution.service';
+export * from './student.service';
+export * from './teacher.service';
+export * from './team.service';
+export * from './vm.service';
